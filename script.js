7f47a26b3ffa28ee798c46e30f51c8b4c35446d7
ChristyDaviCole <christydavicole@gmail.com>
2026-08-29 22:21:03 -0500

    Phase 1: Character Creation (Bindings & Variables)

---
+++ script.js
@@ -0,0 +1,25 @@
+//Phase 1: Character Creation (Bindings & Variables)
+
+/*Define three contants (const) that cannot change:
+the character's name
+their homeWorld
+their baseClass (e.g., "Warrior or "Mage").*/
+
+const characterName = "Chad";
+const homeWorld = "ESPN";
+const baseClass = "Athlete";
+
+/*Define two variables (let) that can change:
+currentHealth and experiencePoints.
+Initialize these with appropriate values 
+(e.g., 100 for health, 0 for experience).*/
+
+let currentHealth = 100;
+let experiencePoints = 0;
+
+/*Use console.log to print a summary of the character’s
+starting stats.*/
+
+console.log("Chad's current health is: " + currentHealth + " and his XP is " + experiencePoints);
+
+//Journal Prompt 1
